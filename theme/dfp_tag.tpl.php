4e93799d54a7b5a3651983705c1ac55fcaf76b4e
Bogdan1988 <bogdan.tur1988@gmail.com>
2013-11-15 11:37:15 +0200

    Added dfp resposive ads module

---
+++ theme/dfp_tag.tpl.php
@@ -0,0 +1,19 @@
+<?php
+  /**
+   * @file
+   * Dfp responsive template for dfp tags.
+   */
+?>
+
+<div <?php print drupal_attributes($placeholder_attributes) ?>>
+  <?php if (isset($slug)):
+    print drupal_render($slug);
+  endif; ?>
+  <script type="text/javascript">
+    if (jQuery(window).width() <?php echo $layout_value;?>) {
+      googletag.cmd.push(function () {
+        googletag.display("<?php print $tag->placeholder_id ?>");
+      });
+    }
+  </script>
+</div>
